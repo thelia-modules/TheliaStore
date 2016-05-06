06c3e32c52db4614c74e34a84fe3dad9bb83bf64
Julien Vigouroux <jvigouroux@openstudio.fr>
2016-05-06 15:42:21 +0200

    wip integration

--- TheliaStore.php
+++ TheliaStore.php
@@ -37,20 +37,21 @@ class TheliaStore extends BaseModule
     static function getApi(){
 
         //local config
+        /*
         $client = new Client(
             "502F9AF505B57FE50C7AA9922",
             "6C94E81DA7FF0FD12CC0179FD36B2DB706E23835A1DEE0BF",
             "http://127.0.0.1/thelia-marketplace/web"
         );
+        */
 
-        /*
         //Online config
         $client = new Client(
             "100FBFED0B742F288013F1ED1",
             "64285C2A60E9F941A7B8EB868A918032C07CDD0C1DD184FB",
             "http://thelia-marketplace.openstudio-lab.com"
         );
-        */
+
         return $client;
     }
 
