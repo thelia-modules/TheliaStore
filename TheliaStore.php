97d526271462a858ef33b0ee8163a9aba77bd9d6
Julien Vigouroux <jvigouroux@openstudio.fr>
2016-06-17 10:15:45 +0200

    update gitignore

--- TheliaStore.php
+++ TheliaStore.php
@@ -21,10 +21,6 @@ use Thelia\Install\Database;
 
 class TheliaStore extends BaseModule
 {
-    /*
-     * TODO :
-     * content-detail.html à modifier, design, image etc ...
-     */
 
     /** @var string */
     const DOMAIN_NAME = 'theliastore';
